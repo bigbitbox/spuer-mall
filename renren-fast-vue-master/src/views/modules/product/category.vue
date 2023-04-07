3453d9ff86ec453102bbd919758671584091d042
rui <ungatz@hotmail.com>
2023-04-07 22:18:21 +0800

    :bookmark: 46  配置网关 笔记：网关的配置 lb://  网关的重写  网关的适配 前端请求路径的更改 人人开发平台的运用

---
+++ renren-fast-vue-master/src/views/modules/product/category.vue
@@ -0,0 +1,30 @@
+<template>
+<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+    export default {
+        name: 'category',
+        data() {
+      return {
+        data: [ ],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      }
+    },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
