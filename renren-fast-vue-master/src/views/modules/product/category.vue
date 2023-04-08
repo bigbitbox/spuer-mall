22ce49b3c76897e487dcdcc90c0ada0b3a4386b2
rui <ungatz@hotmail.com>
2023-04-08 21:29:35 +0800

    :bookmark: 47  完成跨域配置

--- renren-fast-vue-master/src/views/modules/product/category.vue
+++ renren-fast-vue-master/src/views/modules/product/category.vue
@@ -1,30 +1,37 @@
 <template>
-<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
 </template>
 
 <script>
-    export default {
-        name: 'category',
-        data() {
-      return {
-        data: [ ],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
-      };
-    },
-    methods: {
-      handleNodeClick(data) {
-        console.log(data);
+export default {
+  name: 'category',
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
       }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
     },
-        mounted() {
-
-        }
+    getMenus() {
+      this.$http.get('/product/category').then(res => {
+        console.log(res.data);
+        this.data = res.data;
+      });
     }
+  },
+  created() {
+    this.getMenus();
+  },
+  mounted() {
+
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
