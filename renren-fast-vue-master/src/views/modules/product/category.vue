f64cc7460d2b2ef69ab7b3869ec6900a9cb06419
rui <ungatz@hotmail.com>
2023-04-09 22:13:02 +0800

    :bookmark: 51  完成完整的删除系统

--- renren-fast-vue-master/src/views/modules/product/category.vue
+++ renren-fast-vue-master/src/views/modules/product/category.vue
@@ -1,25 +1,18 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false" :show-checkbox="true" node-key="catId">
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" :expand-on-click-node="false"
+    :show-checkbox="true" node-key="catId" :default-expanded-keys="expandedKey">
     <!-- node代表当前结点（是否展开等信息，element-ui自带属性），data是结点数据，是自己的数据。 -->
     <span class="custom-tree-node" slot-scope="{ node, data }">
-        <span>{{ node.label }}</span>
-        <span>
-          <el-button
-            v-if="node.level <=2"
-            type="text"
-            size="mini"
-            @click="() => append(data)">
-            Append
-          </el-button>
-          <el-button
-            v-if="node.childNodes.length==0"
-            type="text"
-            size="mini"
-            @click="() => remove(node, data)">
-            Delete
-          </el-button>
-        </span>
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+          Append
+        </el-button>
+        <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
+          Delete
+        </el-button>
       </span>
+    </span>
   </el-tree>
 </template>
 
@@ -28,6 +21,7 @@ export default {
   name: 'category',
   data() {
     return {
+      expandedKey: [],
       menus: [],
       defaultProps: {
         children: 'children',
@@ -42,7 +36,33 @@ export default {
       console.log("append", data);
     },
     remove(node, data) {
-      console.log("remove", node, data);
+      let ids = [data.catId];
+      this.$confirm(`是否删除【${data.name}】菜单？`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => (
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(() => {
+          console.log("删除成功...")
+          //刷新出菜单
+          this.getMenus();
+          //设置默认展开的菜单
+          this.expandedKey.push[node.parent.data.catId];
+          this.$message({
+            message: '删除成功',
+            type: 'success'
+          })
+        })
+      )).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     },
 
     handleNodeClick(data) {
